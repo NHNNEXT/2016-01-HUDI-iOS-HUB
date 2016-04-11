e5e17abfea24f806a44031496f616a0d0af29e53
kaskay <kaskay77@gmail.com>
2016-04-11 13:39:09 +0900

    month calendar add

---
+++ moodMon/MDMonthViewController.h
@@ -0,0 +1,15 @@
+//
+//  MDMonthViewController.h
+//  moodMon
+//
+//  Created by 이재성 on 2016. 4. 9..
+//  Copyright © 2016년 Lee Kyu-Won. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDMonthViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *monthLabel;
+@property (strong, nonatomic) IBOutlet UILabel *yearLabel;
+
+@end
--- moodMon/MDYearViewController.h
+++ moodMon/MDYearViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 
-@interface YearViewController : UIViewController
+@interface MDYearViewController : UIViewController
 
 @property (strong, nonatomic) IBOutlet UILabel *yearly;
 
