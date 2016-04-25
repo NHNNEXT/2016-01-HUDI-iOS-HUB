14ba9d5ff0d1e5d9cc43382613eeaeadcf12b64a
Kiboom <cheyugy@naver.com>
2016-04-25 19:40:52 +0900

    선택한 감정을 조합하여 무드몬 배경 설정 (작업중)

---
+++ moodMon/MDMoodColorView.h
@@ -0,0 +1,13 @@
+//
+//  MDMoodColorView.h
+//  moodMon
+//
+//  Created by 김기범 on 2016. 4. 25..
+//  Copyright © 2016년 HUB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDMoodColorView : UIView
+@property NSMutableArray *chosenMoods;
+@end
