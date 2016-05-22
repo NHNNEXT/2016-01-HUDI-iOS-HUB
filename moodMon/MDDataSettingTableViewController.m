1c3b325550ff0981c8cd808bd68e27124bdf0e34
kyoo32 <lkwon0302@gmail.com>
2016-05-22 15:53:55 +0900

    [Q_sprint5] fix #74

--- moodMon/MDDataSettingTableViewController.m
+++ moodMon/MDDataSettingTableViewController.m
@@ -22,13 +22,16 @@
     self.dataManager = [MDDataManager sharedDataManager];
     
     
-    UINavigationItem *navItems = [[UINavigationItem alloc] initWithTitle:@"Filter"];
+    UINavigationItem *navItems = [[UINavigationItem alloc] initWithTitle:@"Data Setting"];
     UIBarButtonItem *doneBtn = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStyleDone target:self action:@selector(dataDidFinish)];
     navItems.rightBarButtonItem = doneBtn;
     NSArray *items =[[NSArray alloc]initWithObjects:navItems,nil];
     [self.navigationController.navigationBar setItems:items];
     
     self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showAlert:) name:@"iCloudSyncStart" object:self];
+
 }
 
 
@@ -53,10 +56,76 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
     
     cell.textLabel.text = tableTitles[indexPath.section][indexPath.row];
+    
+    if(indexPath.section == 0){
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        
+//        UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(selectAllMood)];
+//        [cell addGestureRecognizer:gesture];
+        return cell;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
 
      return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.section != 0) return;
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+     if(cell.accessoryType == UITableViewCellAccessoryNone){
+         cell.accessoryType = UITableViewCellAccessoryCheckmark;
+         [[NSNotificationCenter defaultCenter] postNotificationName:@"iCloudSyncStart" object:self userInfo:@{@"message" : @"iCloud동기화를 시작합니다"}];
+
+     } else {
+         cell.accessoryType = UITableViewCellAccessoryNone;
+     }
+    
+}
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.section != 0) return;
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if(cell.accessoryType == UITableViewCellAccessoryNone){
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"iCloudSyncStart" object:self userInfo:@{@"message" : @"iCloud동기화를 시작합니다"}];
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+}
+
+
+-(UIView*)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section{
+    if(section == 0) return NULL;
+    
+    // 1. The view for the footer
+    UIView* footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 22)];
+    
+    // 2. Set a custom background color and a border
+    footerView.backgroundColor = [UIColor colorWithWhite:0.8f alpha:1.0f];
+    footerView.layer.borderColor = [UIColor colorWithWhite:0.8f alpha:1.0].CGColor;
+    footerView.layer.borderWidth = 1.0;
+    
+    // 3. Add a label
+    UILabel* footerLabel = [[UILabel alloc] init];
+    footerLabel.frame = CGRectMake(5, 2, tableView.frame.size.width - 5, 18);
+    footerLabel.backgroundColor = [UIColor clearColor];
+    footerLabel.textColor = [UIColor blackColor];
+    footerLabel.font = [UIFont boldSystemFontOfSize:16.0];
+    footerLabel.textColor = [UIColor redColor];
+    footerLabel.text = @"모든 데이터를 지웁니다!";
+    footerLabel.textAlignment = NSTextAlignmentLeft;
+    
+    // 4. Add the label to the footer view
+    [footerView addSubview:footerLabel];
+    
+    // 5. Finally return
+    return footerView;
+    
+}
 
 
 -(void)dataDidFinish{
@@ -69,6 +138,11 @@
     
 }
 -(void)showAlert:(NSNotification*)notification{
+    NSDictionary *userInfo = [notification userInfo];
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Data" message:[userInfo objectForKey:@"message"] preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil];
+    [alertController addAction:defaultAction];
+    [self presentViewController:alertController animated:YES completion:nil];
     
 }
 
